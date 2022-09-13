58b0b0ec89018b2b30a2ed5b7428ba662cf3155f
Timur <timurka.zavyalov@mail.ru>
2022-09-13 17:49:52 +0300

    add: replace empty data in rows

--- scripts_for_bash_with_inheritance/noe.py
+++ scripts_for_bash_with_inheritance/noe.py
@@ -52,6 +52,7 @@ class WriteDataFromCsvToJsonNoe(WriteDataFromCsvToJson):
     def __call__(self, *args, **kwargs):
         file_name_save = self.remove_empty_columns_and_rows()
         parsed_data = self.read_file_name_save(file_name_save)
+        parsed_data = self.write_duplicate_containers_in_dict(parsed_data, '', 'not_reversed')
         # os.remove(file_name_save)
         return self.write_list_with_containers_in_file(parsed_data)
 
