841f201c6c97ee36178693e33a32dafe79d451a9
timur <timurka.zavyalov@mail.ru>
2022-11-14 18:52:12 +0500

    feat: duplicate containers have been identified by the container seal

--- scripts_for_bash_with_inheritance/verim.py
+++ scripts_for_bash_with_inheritance/verim.py
@@ -43,7 +43,7 @@ class WriteDataFromCsvToJsonVerim(WriteDataFromCsvToJson):
                             parsed_record['container_type'] = ''
                         parsed_record['shipper_country'] = line[self.ir_shipper_country].strip()
                         parsed_record['city'] = line[self.ir_city].strip()
-                        # parsed_record['container_seal'] = line[self.ir_number_plomb]
+                        parsed_record['container_seal'] = line[self.ir_number_plomb]
                         record = self.add_value_from_data_to_list(line, self.ir_container_number,
                                                                   self.ir_weight_goods, self.ir_package_number, self.ir_goods_name_rus,
                                                                   self.ir_shipper, self.ir_consignee,
@@ -54,58 +54,63 @@ class WriteDataFromCsvToJsonVerim(WriteDataFromCsvToJson):
                 self.write_data_before_containers_in_one_column(line, context, month_list, "ВЫГРУЗКА ГРУЗА С")
         return parsed_data
 
-    # @staticmethod
-    # def update_values_duplicate_containers(set_index, set_container_seal, parsed_record,
-    #                                        parsed_data, parsed_data_with_duplicate_containers, record):
-    #     key_list = list(parsed_data[list(set_index)[0]].keys())
-    #     val_list = list(parsed_data[list(set_index)[0]].values())
-    #
-    #     positions = [i for i, d in enumerate(val_list) if d == '']
-    #     for position in positions:
-    #         parsed_data[list(set_index)[0]][key_list[position]] = parsed_record[key_list[position]]
-    #     set_index.pop()
-    #     set_container_seal.pop()
-    #     parsed_data_with_duplicate_containers.append(record)
-    #
-    # def find_duplicate_containers(self, line, keys_list, values_list, values, parsed_record, context, set_index,
-    #                               set_container_seal, list_last_value, index, is_duplicate_containers_in_line):
-    #     for key, value in zip(keys_list, values_list):
-    #         if value == values:
-    #             try:
-    #                 set_index.add(index)
-    #                 set_container_seal.add(line["container_seal"])
-    #                 is_duplicate_containers_in_line = True
-    #                 context[key] = list_last_value[key]
-    #             except KeyError:
-    #                 continue
-    #         else:
-    #             parsed_record[key] = value
-    #         if value != values:
-    #             list_last_value[key] = value
-    #         return self.merge_two_dicts(context, parsed_record)
-    #
-    # def write_duplicate_containers_in_dict(self, parsed_data, values):
-    #     parsed_data_with_duplicate_containers = []
-    #     context = {}
-    #     list_last_value = {}
-    #     set_index = set()
-    #     set_container_seal = set()
-    #     is_duplicate_containers_in_line = False
-    #     for index, line in enumerate(parsed_data):
-    #         keys_list = list(line.keys())
-    #         values_list = list(line.values())
-    #         parsed_record = {}
-    #         record = self.find_duplicate_containers(line, keys_list, values_list, values, parsed_record, context, set_index,
-    #                               set_container_seal, list_last_value, index, is_duplicate_containers_in_line)
-    #         if not is_duplicate_containers_in_line:
-    #             self.update_values_duplicate_containers(set_index, set_container_seal, parsed_record,
-    #                                        parsed_data, parsed_data_with_duplicate_containers, record)
-    #     return parsed_data_with_duplicate_containers
+    @staticmethod
+    def update_values_duplicate_containers(set_index, index, parsed_data, is_reversed):
+        key_list = list(parsed_data[list(set_index)[0]].keys())
+        val_list = list(parsed_data[list(set_index)[0]].values())
+
+        positions = [i for i, d in enumerate(val_list) if d == '']
+        for index_container in list(set_index):
+            for position in positions:
+                parsed_data[index_container][key_list[position]] = parsed_data[index][key_list[position]] \
+                    if is_reversed else parsed_data[index - len(set_index) - 1][key_list[position]]
+            set_index.pop()
+
+    @staticmethod
+    def find_duplicate_containers(is_duplicate_containers_in_line, is_reversed, *args):
+        for key, value in zip(args[1], args[2]):
+            if value == args[3]:
+                try:
+                    args[6].add(args[8])
+                    is_duplicate_containers_in_line = True
+                    if args[0]["container_seal"] == list(args[9].values())[-1] and list(args[9].keys())[-1] != '':
+                        is_reversed = False
+                    args[5][key] = args[7][key]
+                except (KeyError, IndexError):
+                    continue
+            else:
+                args[4][key] = value
+            if value != args[3]:
+                args[7][key] = value
+        return is_duplicate_containers_in_line, is_reversed
+
+    def write_duplicate_containers_in_dict(self, parsed_data, values, **kwargs):
+        context = {}
+        list_last_value = {}
+        set_index = set()
+        is_reversed = True
+        last_container_seal_and_container_dict = {}
+        for index, line in enumerate(parsed_data):
+            if line["container_number"] == '' and line["container_seal"] == '' and line['container_type'] == '' \
+                    and line['container_size'] == '':
+                raise ValueError(f'Container_seal is empty on row {index}')
+            is_duplicate_containers_in_line = False
+            keys_list = list(line.keys())
+            values_list = list(line.values())
+            parsed_record = {}
+            is_duplicate_containers_in_line, is_reversed = \
+                self.find_duplicate_containers(is_duplicate_containers_in_line, is_reversed, line, keys_list,
+                                               values_list, values, parsed_record, context, set_index,
+                                               list_last_value, index, last_container_seal_and_container_dict)
+            if not is_duplicate_containers_in_line and set_index:
+                self.update_values_duplicate_containers(set_index, index, parsed_data, is_reversed)
+            last_container_seal_and_container_dict[line["container_number"]] = line["container_seal"]
+        return parsed_data
 
     def __call__(self, *args, **kwargs):
         file_name_save = self.remove_empty_columns_and_rows()
         parsed_data = self.read_file_name_save(file_name_save)
-        parsed_data = self.write_duplicate_containers_in_dict(parsed_data, '', 'not_reversed')
+        parsed_data = self.write_duplicate_containers_in_dict(parsed_data, '')
         # os.remove(file_name_save)
         return self.write_list_with_containers_in_file(parsed_data)
 
