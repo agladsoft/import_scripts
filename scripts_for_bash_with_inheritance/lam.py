e4d43080f66d21e65e5f2df9ff8bad7232635d8c
timur <timurka.zavyalov@mail.ru>
2022-11-16 19:08:35 +0500

    fix: fixed line lime

--- scripts_for_bash_with_inheritance/lam.py
+++ scripts_for_bash_with_inheritance/lam.py
@@ -11,7 +11,7 @@ class WriteDataFromCsvToJsonLam(WriteDataFromCsvToJsonVerim):
     def __call__(self, *args, **kwargs):
         file_name_save = self.remove_empty_columns_and_rows()
         parsed_data = self.read_file_name_save(file_name_save, __file__)
-        parsed_data = self.write_duplicate_containers_in_dict(parsed_data, '', 'not_reversed')
+        parsed_data = self.write_duplicate_containers_in_dict(parsed_data, '')
         # os.remove(file_name_save)
         return self.write_list_with_containers_in_file(parsed_data)
 
