b3bcc97fa877e8f1ad123a1d1d7233bbba5dca06
timurzav <timurka.zavyalov@mail.ru>
2022-12-23 20:10:30 +0500

    added: added refactoring (lancer)

---
+++ scripts_for_bash_with_inheritance/lancer.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from admiral import Admiral
+
+
+class Lancer(Admiral):
+    pass
+
+
+if __name__ == '__main__':
+    parsed_data = Lancer(os.path.abspath(sys.argv[1]), sys.argv[2], __file__)
+    print(parsed_data.main())
