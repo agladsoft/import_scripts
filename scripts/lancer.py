efd051c7c2c2c56dad008c71a48dcf33378bac8e
timur <timurka.zavyalov@mail.ru>
2023-03-14 18:18:34 +0500

    fix: merged branch_refactoring

--- scripts/lancer.py
+++ scripts/lancer.py
@@ -1,24 +1,17 @@
 import os
 import sys
-from admiral import WriteDataFromCsvToJsonAdmiral
-
-month_list = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября",
-              "ноября", "декабря"]
-
-input_file_path = os.path.abspath(sys.argv[1])
-output_folder = sys.argv[2]
+from admiral import Admiral
 
 
-class WriteDataFromCsvToJsonLancer(WriteDataFromCsvToJsonAdmiral):
-
-    def __call__(self, *args, **kwargs):
-        file_name_save = self.remove_empty_columns_and_rows()
-        parsed_data = self.read_file_name_save(file_name_save, __file__)
-        parsed_data = self.write_duplicate_containers_in_dict(parsed_data, '', 'not_reversed')
-        # os.remove(file_name_save)
-        return self.write_list_with_containers_in_file(parsed_data)
+class Lancer(Admiral):
+    pass
 
 
 if __name__ == '__main__':
-    parsed_data = WriteDataFromCsvToJsonLancer(input_file_path, output_folder)
-    print(parsed_data())
+    parsed_data: Lancer = Lancer(os.path.abspath(sys.argv[1]), sys.argv[2], __file__)
+    try:
+        print(parsed_data.main())
+    except (ValueError, ImportError, IndexError, SyntaxError, TypeError, AttributeError) as ex:
+        print("6", file=sys.stderr)
+        sys.exit(6)
+    del parsed_data
