fbadfce8ad8e50946c2670a3c02b416b1490a8bc
timur <timurka.zavyalov@mail.ru>
2023-04-05 16:07:19 +0300

    add: added new lines (sinokor and xinhelu)

---
+++ scripts/sinokor.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from verim import Verim
+
+
+class Sinokor(Verim):
+    pass
+
+
+if __name__ == '__main__':
+    parsed_data: Sinokor = Sinokor(os.path.abspath(sys.argv[1]), sys.argv[2], __file__)
+    try:
+        print(parsed_data.main())
+    except (ValueError, ImportError, IndexError, SyntaxError, TypeError, AttributeError) as ex:
+        print("6", file=sys.stderr)
+        sys.exit(6)
+    del parsed_data
