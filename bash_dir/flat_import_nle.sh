878c7ff1d3aa1c401a72e3ae4fb2f7ea909ec7c1
timur <timurka.zavyalov@mail.ru>
2023-04-25 09:52:06 +0300

    fix: rename a fields

--- bash_dir/flat_import_nle.sh
+++ bash_dir/flat_import_nle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xls_path="${XL_IDP_PATH_IMPORT}/lines_${XL_IMPORT_TERMINAL}/flat_import_nle"z
+xls_path="${XL_IDP_PATH_IMPORT}/lines_${XL_IMPORT_TERMINAL}/flat_import_nle"
 
 done_path="${xls_path}"/done
 if [ ! -d "$done_path" ]; then
