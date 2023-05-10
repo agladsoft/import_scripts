9d4d7e7f4b2c652781eb73429c24a32ec2ec63a8
timur <timurka.zavyalov@mail.ru>
2023-05-10 14:35:30 +0300

    fix: rename maas to mas

--- bash_dir/_all_lines.sh
+++ bash_dir/_all_lines.sh
@@ -26,6 +26,6 @@ ${XL_IDP_ROOT}/bash_dir/line_ucak_line.sh
 ${XL_IDP_ROOT}/bash_dir/line_lancer.sh
 ${XL_IDP_ROOT}/bash_dir/line_ovp.sh
 ${XL_IDP_ROOT}/bash_dir/line_boat_link.sh
-${XL_IDP_ROOT}/bash_dir/line_maas.sh
+${XL_IDP_ROOT}/bash_dir/line_mas.sh
 ${XL_IDP_ROOT}/bash_dir/line_sinokor.sh
 ${XL_IDP_ROOT}/bash_dir/line_xinhelu.sh
