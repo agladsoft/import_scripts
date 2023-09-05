9ea299b647d0ca05704dcbde13525463df3fc6a7
uvens <uventus1992@gmail.com>
2023-09-05 13:31:19 +0300

    fixed parsed add try

--- scripts/parsed.py
+++ scripts/parsed.py
@@ -1,18 +1,17 @@
 import json
-import requests
+import logging
 
-LINES = ['REEL SHIPPING', 'СИНОКОР РУС ООО', 'HEUNG-A LINE CO., LTD', 'MSC', 'SINOKOR']
+import requests
 
 
 class Parsed:
     def __init__(self):
-        # self.df = df
         self.url = "http://service_consignment:8004"
         self.headers = {
             'Content-Type': 'application/json'
         }
 
-    def body(self, row,line):
+    def body(self, row, line):
         data = {
             'line': line,
             'consignment': row['consignment'],
@@ -21,21 +20,25 @@ class Parsed:
         }
         return data
 
-    def get_result(self, row,line):
-        body = self.body(row,line)
+    def get_result(self, row, line):
+        body = self.body(row, line)
         body = json.dumps(body)
-        answer = requests.post(self.url, data=body, headers=self.headers)
-        if answer.status_code != 200:
+        try:
+            answer = requests.post(self.url, data=body, headers=self.headers)
+            if answer.status_code != 200:
+                return None
+            result = answer.json()
+        except Exception as ex:
+            logging.info(f'Ошибка {ex}')
             return None
-        result = answer.json()
         return result
 
-    def get_port(self,row,line):
+    def get_port(self, row, line):
         self.add_new_columns(row)
-        port = self.get_result(row,line)
+        port = self.get_result(row, line)
         self.write_port(row, port)
 
-    def write_port(self,row, port):
+    def write_port(self, row, port):
         row['is_auto_tracking'] = True
         if port:
             row['is_auto_tracking_ok'] = True
@@ -43,6 +46,5 @@ class Parsed:
         else:
             row['is_auto_tracking_ok'] = False
 
-
-    def add_new_columns(self,row):
+    def add_new_columns(self, row):
         row['is_auto_tracking'] = None
