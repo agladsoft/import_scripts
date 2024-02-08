042f54b2b2915678327c4adbc967482daf46d0b8
uvens <uventus1992@gmail.com>
2024-02-08 12:06:59 +0300

    add safetrance line

--- scripts/safetrans.py
+++ scripts/safetrans.py
@@ -1,7 +1,4 @@
-import itertools
-import os
 import sys
-import re
 from __init__ import *
 from arkas import Arkas, telegram
 
@@ -9,21 +6,11 @@ from arkas import Arkas, telegram
 class Safetrance(Arkas):
 
     def parse_ship_and_voyage(self, parsing_row: str, row: list, column: str, context: dict, key: str,
-                              index_ship: int = 0, index_voyage: int = 2) -> None:
+                              index_ship: int = 0, index_voyage: int = 1) -> None:
         """
-        Parsing ship name and voyage in the cells before the table.
+        Method inheritance from Arkas.
         """
-        self.logger_write.info(f"Will parse ship and trip in value '{parsing_row}'...")
-        index: int = 0
-        for ship_voyage in row:
-            position: int = list(DICT_CONTENT_BEFORE_TABLE.values()).index("ship_voyage_in_other_cells")
-            if re.findall(list(DICT_CONTENT_BEFORE_TABLE.keys())[position][0], ship_voyage):
-                if index == index_ship:
-                    context["ship_name"] = self.remove_keys_in_data(ship_voyage)
-                elif index == index_voyage:
-                    context['voyage'] = self.remove_keys_in_data(ship_voyage)
-                index += 1
-        self.logger_write.info(f"context now is {context}")
+        Arkas.parse_ship_and_voyage(self, parsing_row, row, column, context, key, 0, 2)
 
 
 if __name__ == '__main__':
