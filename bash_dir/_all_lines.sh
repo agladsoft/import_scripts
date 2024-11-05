60972525ede0dbea355a88e47216d2763a690a79
uventus <uventus_work@mail.ru>
2024-11-05 18:04:24 +0300

    add : add line EconShip

--- bash_dir/_all_lines.sh
+++ bash_dir/_all_lines.sh
@@ -36,3 +36,4 @@ ${XL_IDP_ROOT}/bash_dir/line_safetrans.sh
 ${XL_IDP_ROOT}/bash_dir/line_united.sh
 ${XL_IDP_ROOT}/bash_dir/line_smart.sh
 ${XL_IDP_ROOT}/bash_dir/line_login.sh
+${XL_IDP_ROOT}/bash_dir/line_econship.sh
---
+++ scripts/econship.py
@@ -0,0 +1,17 @@
+from login import Login
+from __init__ import *
+
+
+class EconShip(Login):
+    pass
+
+
+if __name__ == '__main__':
+    parsed_data: EconShip = EconShip(os.path.abspath(sys.argv[1]), sys.argv[2], __file__)
+    try:
+        print(parsed_data.main())
+    except (ValueError, ImportError, IndexError, SyntaxError, TypeError, AttributeError) as ex:
+        print("6", file=sys.stderr)
+        telegram(f'Ошибка {ex} при обработке файла {parsed_data.input_file_path}')
+        sys.exit(6)
+    del parsed_data
