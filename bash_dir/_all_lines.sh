054221e58df5522f5b2c9359fc528f0ecce8c3a0
uventus <uventus_work@mail.ru>
2025-09-03 10:46:03 +0300

    Добавление новой линии в обработку siv_mena_line

--- bash_dir/_all_lines.sh
+++ bash_dir/_all_lines.sh
@@ -42,3 +42,4 @@ ${XL_IDP_ROOT}/bash_dir/line_vuxx_shipping.sh
 ${XL_IDP_ROOT}/bash_dir/line_mohill.sh
 ${XL_IDP_ROOT}/bash_dir/line_m_line.sh
 ${XL_IDP_ROOT}/bash_dir/line_eccl.sh
+${XL_IDP_ROOT}/bash_dir/line_siv_mena_line.sh
